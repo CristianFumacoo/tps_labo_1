0fcaa6710cd55a575c39b8188fc012278562a5a3
Chris <54670267+CristianFumacoo@users.noreply.github.com>
2021-07-07 11:49:34 -0300

    Add files via upload

---
+++ tp4/TP3_Recu/Controller.h
@@ -0,0 +1,20 @@
+int controller_loadFromText(char* path , LinkedList* pArrayListEmployee);//Listo
+
+int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee);//Listo
+
+int controller_addEmployee(LinkedList* pArrayListEmploye);
+
+int controller_editEmployee(LinkedList* pArrayListEmployee);
+
+int controller_removeEmployee(LinkedList* pArrayListEmployee);//Listo
+
+int controller_ListEmployee(LinkedList* pArrayListEmployee);
+
+int controller_sortEmployee(LinkedList* pArrayListEmployee);
+
+int controller_saveAsText(char* path , LinkedList* pArrayListEmployee);
+
+int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee);
+
+int buscarId(LinkedList*);
+
